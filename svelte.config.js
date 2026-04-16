3649403dc195e47c10039faf86ccf15e659313c2
lorenaromero148j-sudo <lorena.romero148j@gmail.com>
2026-04-15 23:22:40 -0500

    Switch to static adapter and configure paths

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static'; // Cambia adapter-auto por static
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html'
+		}),
+		paths: {
+			base: '/activity-2', // Tu nombre de repositorio
+		}
 	}
 };
 
